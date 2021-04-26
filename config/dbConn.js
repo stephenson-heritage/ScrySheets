b552622a14de276251f3a0298b9aa3a339bedfe5
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2021-04-26 08:07:39 -0400

    connection url

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-let dbConn = process.env.JAWSDB_MARIA_URL ? process.env.JAWSDB_MARIA_URL : {
+
+let dbConn = process.env.MARIA_URL !== undefined ? process.env.MARIA_URL : {
 	host: 'localhost',
 	user: 'root',
 	password: '',
